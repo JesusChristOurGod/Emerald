03b6eea738c1feb1643e24687565b8e08f979e7a
JesusChristOurGod <56786170+JesusChristOurGod@users.noreply.github.com>
2020-11-12 19:53:53 +0300

    Update StructView.swift

--- Emerald/Emerald/StructView.swift
+++ Emerald/Emerald/StructView.swift
@@ -8,21 +8,20 @@
 import SwiftUI
 
 struct StructView: View {
-    var data: [ShapeModel] = [
-//        .init(id: 1, color: .red, startX: 120, startY: 275, currentX: 200, currentY: 350),
-//        .init(id: 2, color: .yellow, startX: 100, startY: 320, currentX: 300, currentY: 470)
-
-    ]
+   @State var data: [ShapeModel] = []
+    @State var selectData: [SelectModel] = []
     @State var changeCounter: Int = 0
     @State var startXPos: CGFloat = 0
     @State var startYPos: CGFloat = 0
     @State var startRealYPos: CGFloat = 0
     @State var startRealXPos: CGFloat = 0
+    @State var drawType: String = ""
     var body: some View {
+        VStack {
         ZStack {
             GeometryReader{ geo in
-                Rectangle().frame(width: 800, height:800, alignment: .center).foregroundColor(.blue).position(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2)
-            }.gesture(DragGesture(minimumDistance: 0, coordinateSpace: .global)
+                Rectangle().frame(width: 800, height:800, alignment: .center).foregroundColor(.gray).position(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2)
+            }.gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
                         .onChanged{ dragGesture in
                             if changeCounter == 0 {
                                 self.startXPos = dragGesture.location.x
@@ -33,36 +32,102 @@ struct StructView: View {
                             self.startRealYPos = dragGesture.location.y
                         }
                         .onEnded{dragGesture in
-                            
+                            if self.drawType == "rectangle" {
+                            data.append(.init( id: .init(), color: colorGenerator(), startX: self.startXPos, startY: self.startYPos, currentX: self.startRealXPos, currentY: self.startRealYPos))
                             self.changeCounter = 0
-                            data.append(.init(id: 3, color: colorGenerator(), startX: startXPos, startY: startYPos, currentX: startRealXPos, currentY: startRealYPos))
+                            }
+                            if self.drawType=="choosing" {
+                                selectData.append(.init(id: .init(), selStartX: self.startXPos, selStartY: self.startYPos, selCurrentX: self.startRealXPos, selCurrentY: self.startRealYPos))
+
+                            }
+                            
                         }
             )
-            
-            
-                
-           
             ForEach(data){ items in
                 ShapeViewer(data: items)
             }
-         
+            ForEach(selectData) { items in
+                SelectViewer(selectData: items)
+            }
+                HStack {
+                    Button(action: {
+                        self.drawType = "rectangle"
+                        
+                    }) {
+                        Text ("Rectangle").frame(width: 100, height: 20)
+                    }
+                    Button(action: {
+                        self.drawType = "choosing"
+                        
+                    }) {
+                        Text ("Choosing").frame(width: 100, height: 20)
+                    }
+                }.position(x: UIScreen.main.bounds.width/2, y: 1000)
+        }
         }
     }
     
     func colorGenerator() -> Color {
-        return  Color.init(red: Double((Int.random(in: 0..<255)/255)), green: Double((Int.random(in: 0..<255)/255)), blue: Double((Int.random(in: 0..<255)/255)))
+        return  Color( red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), opacity: 1.0)
        
+    }
+    
+    
+    func isSelected() -> Bool {
+        var isIt: Bool = false
+        var selectStartX: CGFloat = 0
+        var selectStartY: CGFloat = 0
+        var selectEndX: CGFloat = 0
+        var selectEndY: CGFloat = 0
+        
+        data.forEach { items in
+            if (selectEndX > items.startX && selectEndX < items.currentX) {
+                isIt =  true
+            }
+            if (selectStartX > items.startX && selectStartX < items.currentX) {
+                isIt =  true
+            }
+            
+            
+            if (selectEndX>items.currentX && selectStartX < items.startX && selectEndY>items.currentY && selectStartY < items.startY ) {
+                isIt =  true
+            }
+            if (selectStartX>items.currentX && selectEndX < items.startX && selectStartY>items.currentY && selectEndY < items.startY ) {
+                isIt =  true
+            }
+            
+            
+            if (selectEndX>items.currentX && selectStartX < items.startX && selectEndY<items.currentY && selectEndY>items.startY  && selectStartY < items.startY ) {
+                isIt =  true
+            }
+            if (selectStartX>items.currentX && selectEndX < items.startX && selectStartY<items.currentY && selectStartY>items.startY && selectEndY < items.startY ) {
+                isIt =  true
+            }
+            
+            
+            if (selectEndY>items.currentY && selectEndX > items.currentX && selectStartX < items.startX && selectStartY > items.startY && selectStartY < items.currentY) {
+                isIt =  true
+            }
+            if (selectStartY>items.currentY && selectStartX > items.currentX && selectEndX < items.startX && selectEndY > items.startY && selectEndY < items.currentY) {
+                isIt =  true
+            }
+            
+        }
+        return isIt
     }
 }
 
 struct ShapeModel: Identifiable {
-    var id: Int
+    var id: UUID
     var color: Color
     var startX: CGFloat
     var startY: CGFloat
     var currentX: CGFloat
     var currentY: CGFloat
 }
+
+
+
 
 struct ShapeViewer: View {
     var data: ShapeModel
@@ -72,6 +137,23 @@ struct ShapeViewer: View {
         }.foregroundColor(self.data.color)
     }
 }
+
+struct SelectModel: Identifiable {
+    var id: UUID
+    var selStartX: CGFloat
+    var selStartY: CGFloat
+    var selCurrentX: CGFloat
+    var selCurrentY: CGFloat
+}
+struct SelectViewer:View {
+    var selectData:SelectModel
+    var body: some View {
+        Path{ path in
+            path.addRect(CGRect(x: self.selectData.selStartX, y: self.selectData.selStartY, width: self.selectData.selCurrentX-self.selectData.selStartX, height: self.selectData.selCurrentY-self.selectData.selStartY))
+        }.stroke(Color.blue,style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+}
+
 
 struct StructView_Previews: PreviewProvider {
     static var previews: some View {
